815d7c4a0e50c90cd7c156a40511d2c4a2c880fc
s-takenoshita <s.takenoshita@gmail.com>
2022-04-17 14:53:46 +0900

    db-migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_051222) do
+ActiveRecord::Schema.define(version: 2022_04_17_054721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tournaments", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.date "dates"
+    t.integer "party_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
