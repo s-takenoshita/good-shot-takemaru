e6922c1e772cd89cf363fe2d989971d2b3fffd26
s-takenoshita <s.takenoshita@gmail.com>
2022-04-18 22:28:29 +0900

    new & index

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,2 +1,26 @@
 class TournamentsController < ApplicationController
+  def new
+    @tournament = Tournament.new
+  end
+
+  def create
+    @tournament = Tournament.new(tournament_params)
+    # @tournament.party_id = current_user.id
+    if @tournament.save
+      redirect_to tournaments_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @tournaments = Tournament.all
+  end
+
+  private
+
+  def tournament_params
+    params.require(:tournament).permit(:name, :dates)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
+  resources :tournaments, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
